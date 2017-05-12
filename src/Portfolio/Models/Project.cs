29fd76b4a8bfebb911630b6104adc7f54f69ddef
brandon <bcooper085@gmail.com>
2017-05-12 15:55:27 -0700

    add content structure and styling

--- src/Portfolio/Models/Project.cs
+++ src/Portfolio/Models/Project.cs
@@ -14,6 +14,9 @@ namespace Portfolio.Models
     {
         public string name { get; set; }
         public int stargazers_count { get; set; }
+        public int id { get; set; }
+        public string html_url { get; set; }
+        public string description { get; set; }
 
         public static List<Project> GetProjects()
         {
